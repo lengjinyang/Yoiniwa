808e928324edaa23fc2261ac2bccb681886f22c3
Ayaka <1505091241@qq.com>
2026-08-09 16:26:20 +0800

    v0.01a

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,21 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: './',
-  build: { outDir: 'dist', emptyOutDir: true },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rolldownOptions: {
+      output: {
+        codeSplitting: {
+          groups: [
+            {
+              name: 'react-runtime',
+              test: /node_modules[\\/](?:react|react-dom|scheduler)[\\/]/,
+              priority: 30,
+            },
+          ],
+        },
+      },
+    },
+  },
 });
